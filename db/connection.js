9a0c4f70a1a46166734ae0e6951de09963c2c0fa
Kuldeep Gupta <kuldeepguptaa17@gmail.com>
2022-11-29 20:04:03 +0530

    Updated db url

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 console.log(process.env.DATABASE_URL)
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true},{useUnifiedTopology:true})
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.dj2b9xh.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri,{useNewUrlParser:true},{useUnifiedTopology:true})
 .then(()=>{
     console.log("Connection established")
 })

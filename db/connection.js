fa9391336bbe893215a29cafe9befde264add4d6
Kuldeep Gupta <kuldeepguptaa17@gmail.com>
2024-06-19 19:45:52 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.dj2b9xh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.dj2b9xh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose
   .connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true })
   .then(() => {
